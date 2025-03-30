04ec638dd678b5fe363a75f98157e8ff482ca44c
Ranigal <evgeha.ra@yandex.ru>
2025-03-30 20:42:30 +0300

    Удалил main.py

--- run.py
+++ run.py
@@ -2,10 +2,17 @@
 """
     Запуск сервера
 """
+import os
 
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run()
+    """
+        для тестов на tuna (тест-деплой в интернет)
+        Потом еще немного изменим для полноценного деплоя на glitch
+    """
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    # app.run()
